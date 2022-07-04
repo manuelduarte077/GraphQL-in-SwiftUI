2d0817a2bfc66f70f327d8acc4fa43959f2efe3a
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-04 01:18:33 -0600

    Initial Commit

---
+++ GraphQLSwiftUI/GraphQLSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLSwiftUIApp.swift
+//  GraphQLSwiftUI
+//
+//  Created by Manuel Duarte on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
